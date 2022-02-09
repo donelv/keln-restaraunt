db2f8ded94b305612cc618340f409e3282d179f1
donelv <burndog7@gmail.com>
2022-02-09 22:59:56 +0300

    changed order page

--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -4,31 +4,43 @@ const OrderPage = () => {
   return (
     <form className="form">
       <div>
-        <input className="form__input"></input>
+        <input className="form__input" placeholder="Имя"></input>
       </div>
       <div>
-        <input className="form__input"></input>
+        <input className="form__input" placeholder="Телефон"></input>
       </div>
       <div>
-        <input className="form__input"></input>
+        <input className="form__input" placeholder="Адрес"></input>
       </div>
       <div className="form__wrapper-input">
         <div>
-          <input className="form__input form__input-small"></input>
+          <input
+            className="form__input form__input-small"
+            placeholder="Подъезд"
+          ></input>
         </div>
         <div>
-          <input className="form__input form__input-small"></input>
+          <input
+            className="form__input form__input-small"
+            placeholder="Домофон"
+          ></input>
         </div>
         <div>
-          <input className="form__input form__input-small"></input>
+          <input
+            className="form__input form__input-small"
+            placeholder="Кв./Офис"
+          ></input>
         </div>
         <div>
-          <input className="form__input form__input-small"></input>
+          <input
+            className="form__input form__input-small"
+            placeholder="Этаж"
+          ></input>
         </div>
       </div>
 
       <div>
-        <input className="form__input"></input>
+        <input className="form__input" placeholder="Комментарий"></input>
       </div>
       <div>
         <button className="form__button">Подтвердить заказ</button>
