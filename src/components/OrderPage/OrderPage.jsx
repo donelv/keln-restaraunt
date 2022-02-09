e1cb0a848b15cea05b789a1444297676d6459a6f
donelv <burndog7@gmail.com>
2022-02-10 01:16:38 +0300

    changed order page

--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -2,68 +2,70 @@ import React from 'react'
 import './OrderPage.css'
 const OrderPage = () => {
   return (
-    <form className="form">
-      <div className="form__group">
-        <input className="form__input" spellCheck="false" required></input>
-        <label className="form__label">Имя</label>
-      </div>
-      <div className="form__group">
-        <input
-          className="form__input"
-          spellCheck="false"
-          required
-          type="tel"
-          pattern="+7([0-9]{3}) [0-9]{3}-[0-9]{2}-[0-9]{2}"
-        ></input>
-        <label className="form__label">Телефон</label>
-      </div>
-      <div className="form__group">
-        <input className="form__input" spellCheck="false" required></input>
-        <label className="form__label">Адрес</label>
-      </div>
-      <div className="form__wrapper-input">
+    <div className="divka">
+      <form className="form">
         <div className="form__group">
-          <input
-            className="form__input form__input-small"
-            spellCheck="false"
-            required
-          ></input>
-          <label className="form__label">Подъезд</label>
+          <input className="form__input" spellCheck="false" required></input>
+          <label className="form__label">Имя</label>
         </div>
         <div className="form__group">
           <input
-            className="form__input form__input-small"
+            className="form__input"
             spellCheck="false"
             required
+            type="tel"
+            pattern="+7([0-9]{3}) [0-9]{3}-[0-9]{2}-[0-9]{2}"
           ></input>
-          <label className="form__label">Домофон</label>
+          <label className="form__label">Телефон</label>
         </div>
         <div className="form__group">
-          <input
-            className="form__input form__input-small"
-            spellCheck="false"
-            required
-          ></input>
-          <label className="form__label">Кв./Офис</label>
+          <input className="form__input" spellCheck="false" required></input>
+          <label className="form__label">Адрес</label>
         </div>
-        <div className="form__group">
-          <input
-            className="form__input form__input-small"
-            spellCheck="false"
-            required
-          ></input>
-          <label className="form__label">Этаж</label>
+        <div className="form__wrapper-input">
+          <div className="form__group">
+            <input
+              className="form__input form__input-small"
+              spellCheck="false"
+              required
+            ></input>
+            <label className="form__label">Подъезд</label>
+          </div>
+          <div className="form__group">
+            <input
+              className="form__input form__input-small"
+              spellCheck="false"
+              required
+            ></input>
+            <label className="form__label">Домофон</label>
+          </div>
+          <div className="form__group">
+            <input
+              className="form__input form__input-small"
+              spellCheck="false"
+              required
+            ></input>
+            <label className="form__label">Кв./Офис</label>
+          </div>
+          <div className="form__group">
+            <input
+              className="form__input form__input-small"
+              spellCheck="false"
+              required
+            ></input>
+            <label className="form__label">Этаж</label>
+          </div>
         </div>
-      </div>
 
-      <div className="form__group">
-        <input className="form__input" spellCheck="false" required></input>
-        <label className="form__label">Комментарий</label>
-      </div>
-      <div>
-        <button className="form__button">Подтвердить заказ</button>
-      </div>
-    </form>
+        <div className="form__group">
+          <input className="form__input" spellCheck="false" required></input>
+          <label className="form__label">Комментарий</label>
+        </div>
+        <div>
+          <button className="form__button">Подтвердить заказ</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
