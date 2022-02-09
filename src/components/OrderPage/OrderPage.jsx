d1f23d9bbb23a23fb7e331ac3ef512ea159bf7a8
donelv <burndog7@gmail.com>
2022-02-10 00:32:33 +0300

    changed order page

--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -3,64 +3,56 @@ import './OrderPage.css'
 const OrderPage = () => {
   return (
     <form className="form">
-      <div>
-        <input
-          className="form__input"
-          placeholder="Имя"
-          spellcheck="false"
-        ></input>
+      <div className="form__group">
+        <input className="form__input" spellCheck="false" required></input>
+        <label className="form__label">Имя</label>
       </div>
-      <div>
-        <input
-          className="form__input"
-          placeholder="Телефон"
-          spellcheck="false"
-        ></input>
+      <div className="form__group">
+        <input className="form__input" spellCheck="false" required></input>
+        <label className="form__label">Телефон</label>
       </div>
-      <div>
-        <input
-          className="form__input"
-          placeholder="Адрес"
-          spellcheck="false"
-        ></input>
+      <div className="form__group">
+        <input className="form__input" spellCheck="false" required></input>
+        <label className="form__label">Адрес</label>
       </div>
       <div className="form__wrapper-input">
-        <div>
+        <div className="form__group">
           <input
             className="form__input form__input-small"
-            placeholder="Подъезд"
-            spellcheck="false"
+            spellCheck="false"
+            required
           ></input>
+          <label className="form__label">Подъезд</label>
         </div>
-        <div>
+        <div className="form__group">
           <input
             className="form__input form__input-small"
-            placeholder="Домофон"
-            spellcheck="false"
+            spellCheck="false"
+            required
           ></input>
+          <label className="form__label">Домофон</label>
         </div>
-        <div>
+        <div className="form__group">
           <input
             className="form__input form__input-small"
-            placeholder="Кв./Офис"
-            spellcheck="false"
+            spellCheck="false"
+            required
           ></input>
+          <label className="form__label">Кв./Офис</label>
         </div>
-        <div>
+        <div className="form__group">
           <input
             className="form__input form__input-small"
-            placeholder="Этаж"
-            spellcheck="false"
+            spellCheck="false"
+            required
           ></input>
+          <label className="form__label">Этаж</label>
         </div>
       </div>
 
-      <div>
-        <input
-          className="form__input"
-          placeholder="Комментарий"
-          spellcheck="false"
-        ></input>
+      <div className="form__group">
+        <input className="form__input" spellCheck="false" required></input>
+        <label className="form__label">Комментарий</label>
       </div>
       <div>
         <button className="form__button">Подтвердить заказ</button>
