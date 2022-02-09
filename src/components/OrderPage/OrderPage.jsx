1d21525f8438334d9807c486e284b61d0e7cf717
donelv <burndog7@gmail.com>
2022-02-09 23:17:46 +0300

    changed order page

--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -4,43 +4,63 @@ const OrderPage = () => {
   return (
     <form className="form">
       <div>
-        <input className="form__input" placeholder="Имя"></input>
+        <input
+          className="form__input"
+          placeholder="Имя"
+          spellcheck="false"
+        ></input>
       </div>
       <div>
-        <input className="form__input" placeholder="Телефон"></input>
+        <input
+          className="form__input"
+          placeholder="Телефон"
+          spellcheck="false"
+        ></input>
       </div>
       <div>
-        <input className="form__input" placeholder="Адрес"></input>
+        <input
+          className="form__input"
+          placeholder="Адрес"
+          spellcheck="false"
+        ></input>
       </div>
       <div className="form__wrapper-input">
         <div>
           <input
             className="form__input form__input-small"
             placeholder="Подъезд"
+            spellcheck="false"
           ></input>
         </div>
         <div>
           <input
             className="form__input form__input-small"
             placeholder="Домофон"
+            spellcheck="false"
           ></input>
         </div>
         <div>
           <input
             className="form__input form__input-small"
             placeholder="Кв./Офис"
+            spellcheck="false"
           ></input>
         </div>
         <div>
           <input
             className="form__input form__input-small"
             placeholder="Этаж"
+            spellcheck="false"
           ></input>
         </div>
       </div>
 
       <div>
-        <input className="form__input" placeholder="Комментарий"></input>
+        <input
+          className="form__input"
+          placeholder="Комментарий"
+          spellcheck="false"
+        ></input>
       </div>
       <div>
         <button className="form__button">Подтвердить заказ</button>
