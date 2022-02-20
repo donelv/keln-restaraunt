678ebb6bdfaeaba2ec15efbc922ff3451073080c
donelv <burndog7@gmail.com>
2022-02-20 21:47:54 +0300

    connected to db

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,30 +9,54 @@ const Cart = (props) => {
   return (
     <div>
       <Header whatPage={'Корзина'}></Header>
-      <main className="main-cart">
-        <div className="container">
-          {props.sum === 0 ? (
-            <EmptyCart />
-          ) : (
-            <div className="row">
-              <CartItemContainer />
-              <div className="col-xl-5 offset-xl-2 col-lg-6 offset-lg-1 col-6">
-                <div className="main-cart-sum">
-                  <span>Итогo: {props.sum}₽</span>
-                </div>
+      <div className="main-cart">
+        {props.sum === 0 ? (
+          <EmptyCart />
+        ) : (
+          <>
+            <CartItemContainer />
+            <div className="main-cart__button-wrapper">
+              <div className="main-cart-sum">
+                <span>Итогo: {props.sum}₽</span>
               </div>
-              <div className="col-xl-3 col-lg-4 col-6">
-                <button className="main-cart-order_button">
-                  <Link to="/cart/order">Оформление заказа</Link>
-                </button>
-              </div>
+              <button className="main-cart-order_button">
+                <Link to="/cart/order">Оформление заказа</Link>
+              </button>
             </div>
-          )}
-        </div>
-      </main>
+          </>
+        )}
+      </div>
     </div>
   )
 }
+// const Cart = (props) => {
+//   return (
+//     <div>
+//       <Header whatPage={'Корзина'}></Header>
+//       <main className="main-cart">
+//         <div className="container">
+//           {props.sum === 0 ? (
+//             <EmptyCart />
+//           ) : (
+//             <div className="row">
+//               <CartItemContainer />
+//               <div className="col-xl-5 offset-xl-2 col-lg-6 offset-lg-1 col-6">
+//                 <div className="main-cart-sum">
+//                   <span>Итогo: {props.sum}₽</span>
+//                 </div>
+//               </div>
+//               <div className="col-xl-3 col-lg-4 col-6">
+//                 <button className="main-cart-order_button">
+//                   <Link to="/cart/order">Оформление заказа</Link>
+//                 </button>
+//               </div>
+//             </div>
+//           )}
+//         </div>
+//       </main>
+//     </div>
+//   )
+// }
 let mapStateToProps = (state) => {
   return {
     sum: state.cart.sum,
--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import defaultPhoto from '../../assets/img/food/leberhause.jpg'
-import decreaseItemIcon from '../../assets/img/delete.svg'
-import increaseItemIcon from '../../assets/img/add.svg'
+import decreaseItemIcon from '../../assets/img/minus.svg'
+import increaseItemIcon from '../../assets/img/plus.svg'
 import deleteItemIcon from '../../assets/img/cancel.svg'
 const CartItem = (props) => {
   const handleClick = (increaseOrDecrease) => {
@@ -13,47 +13,44 @@ const CartItem = (props) => {
     props.deleteItem(props.id)
   }
   return (
-    <div className="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
-      <div className="main-cart-item">
-        <div className="main-cart-item-image">
-          <img src={defaultPhoto} alt={props.name} />
-        </div>
-
-        <div className="main-cart-item-name">
-          <div>{props.name}</div>
-          <p>{props.description}</p>
-        </div>
-        <div className="main-cart-item-wrap">
-          <div className="main-cart-item-wrap-amount">
-            {props.amount <= 0 ? (
-              handleDelete()
-            ) : (
-              <div>
-                <button
-                  className="main-cart-item-wrap-amount-button"
-                  onClick={() => handleClick(false)}
-                >
-                  <img src={decreaseItemIcon} />
-                </button>
-                <span>{props.amount}</span>
-                <button
-                  className="main-cart-item-wrap-amount-button"
-                  onClick={() => handleClick(true)}
-                >
-                  <img src={increaseItemIcon} />
-                </button>
-              </div>
-            )}
-          </div>
-
-          <div className="main-cart-item-wrap-price">
-            <span>{props.price}₽</span>
-          </div>
-        </div>
-        <button className="main-cart-item-delete" onClick={handleDelete}>
-          <img src={deleteItemIcon} alt="Закрыть" />
-        </button>
+    <div className="main-cart-item">
+      <div className="main-cart-item__image">
+        <img src={defaultPhoto} alt={props.name} />
       </div>
+
+      <div className="main-cart-item__name">
+        <div>{props.name}</div>
+        <p>{props.description}</p>
+      </div>
+      <div className="main-cart-item__ap-grid">
+        <div className="main-cart-item-wrap-price">
+          <span>{props.price}₽</span>
+        </div>
+        <div className="main-cart-item-wrap-amount">
+          {props.amount <= 0 ? (
+            handleDelete()
+          ) : (
+            <div>
+              <button
+                className="main-cart-item-wrap-amount-button"
+                onClick={() => handleClick(false)}
+              >
+                <img src={decreaseItemIcon} />
+              </button>
+              <span>{props.amount}</span>
+              <button
+                className="main-cart-item-wrap-amount-button"
+                onClick={() => handleClick(true)}
+              >
+                <img src={increaseItemIcon} />
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+      {/* <button className="main-cart-item-delete" onClick={handleDelete}>
+        <img src={deleteItemIcon} alt="Закрыть" />
+      </button> */}
     </div>
   )
 }
