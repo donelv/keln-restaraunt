5b5791fbd0d59ac661b97e8cdffcedebe6e11cfe
donelv <burndog7@gmail.com>
2022-02-20 16:00:12 +0300

    connected to db

--- src/db.js
+++ src/db.js
@@ -7,13 +7,13 @@ import {
   orderBy,
 } from '@firebase/firestore'
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyA5cf6VkZg1jGByczbWEg5XupNknveZUzs',
+  authDomain: 'keln-restaraunt.firebaseapp.com',
+  databaseURL: 'https://keln-restaraunt-default-rtdb.firebaseio.com',
+  projectId: 'keln-restaraunt',
+  storageBucket: 'keln-restaraunt.appspot.com',
+  messagingSenderId: '724905727936',
+  appId: '1:724905727936:web:94e201b89a37b579569a25',
 }
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
